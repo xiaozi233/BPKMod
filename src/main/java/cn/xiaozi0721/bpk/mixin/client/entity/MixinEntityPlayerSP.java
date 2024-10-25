288d92498ab3ca93d8f2f6a7162aa59494ee7038
xiaozi0721 <1793116683@qq.com>
2024-10-25 23:56:48 +0800

    update version readme

--- src/main/java/cn/xiaozi0721/bpk/mixin/client/entity/MixinEntityPlayerSP.java
+++ src/main/java/cn/xiaozi0721/bpk/mixin/client/entity/MixinEntityPlayerSP.java
@@ -3,7 +3,6 @@ package cn.xiaozi0721.bpk.mixin.client.entity;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
-import net.minecraft.util.MovementInput;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
