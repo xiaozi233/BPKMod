117b027b7fca4cb5377ea4c34a552262ea4bdefd
xiaozi0721 <1793116683@qq.com>
2024-10-26 16:17:24 +0800

    fix config and you can sprint backward now(default disabled)

--- src/main/java/cn/xiaozi0721/bpk/config/GeneralConfig.java
+++ src/main/java/cn/xiaozi0721/bpk/config/GeneralConfig.java
@@ -2,9 +2,26 @@ package cn.xiaozi0721.bpk.config;
 
 import cn.xiaozi0721.bpk.BPK;
 import net.minecraftforge.common.config.Config;
+import net.minecraftforge.common.config.ConfigManager;
+import net.minecraftforge.fml.client.event.ConfigChangedEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Config(modid = BPK.MOD_ID, category = "BPK Mod")
 public class GeneralConfig {
+    @Mod.EventBusSubscriber(modid = BPK.MOD_ID)
+    public static class ConfigSyncHandler {
+        @SubscribeEvent
+        public static void onConfigChanged(ConfigChangedEvent.OnConfigChangedEvent event) {
+            if (event.getModID().equals(BPK.MOD_ID)) {
+                ConfigManager.sync(BPK.MOD_ID, Config.Type.INSTANCE);
+            }
+        }
+    }
+
     @Config.Comment({"Simulate new touch strafe.", "If enabled, the strafe angle will be changed to 11.48(arccos(0.98))"})
     public static boolean isNewTouch = false;
+
+    @Config.Comment({"If enabled, you could sprint even if you are backward."})
+    public static boolean isSprintBackward = false;
 }
--- src/main/java/cn/xiaozi0721/bpk/mixin/client/entity/MixinEntityPlayerSP.java
+++ src/main/java/cn/xiaozi0721/bpk/mixin/client/entity/MixinEntityPlayerSP.java
@@ -3,11 +3,15 @@ package cn.xiaozi0721.bpk.mixin.client.entity;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.util.MovementInput;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
+
+import static cn.xiaozi0721.bpk.config.GeneralConfig.isSprintBackward;
+
 
 @Mixin(EntityPlayerSP.class)
 public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
@@ -15,6 +19,11 @@ public abstract class MixinEntityPlayerSP extends AbstractClientPlayer {
 
     public MixinEntityPlayerSP(World worldIn, GameProfile playerProfile) {
         super(worldIn, playerProfile);
+    }
+
+    @Redirect(method = "onLivingUpdate",at = @At(value = "FIELD", target = "Lnet/minecraft/util/MovementInput;moveForward:F", ordinal = 5))
+    private float setmoveForward(MovementInput movementInput){
+        return isSprintBackward && movementInput.moveForward !=0 ? 1 : movementInput.moveForward;
     }
 
     @Redirect(method = "onLivingUpdate", at = @At(value = "FIELD", target = "Lnet/minecraft/client/entity/EntityPlayerSP;collidedHorizontally:Z"))
