842be30d318858cfcc3c597c093f07fd7d917edc
xiaozi0721 <1793116683@qq.com>
2025-08-24 13:27:39 +0800

    fix top and bottom trapdoor's collision box

--- src/main/java/cn/xiaozi0721/bpk/mixin/block/MixinBlockTrapDoor.java
+++ src/main/java/cn/xiaozi0721/bpk/mixin/block/MixinBlockTrapDoor.java
@@ -15,6 +15,8 @@ public abstract class MixinBlockTrapDoor extends Block {
     @Shadow @Final @Mutable protected static AxisAlignedBB WEST_OPEN_AABB = new AxisAlignedBB(0.8175D, 0.0D, 0.0D, 1.0D, 1.0D, 1.0D);
     @Shadow @Final @Mutable protected static AxisAlignedBB SOUTH_OPEN_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 1.0D, 0.1825D);
     @Shadow @Final @Mutable protected static AxisAlignedBB NORTH_OPEN_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.8175D, 1.0D, 1.0D, 1.0D);
+    @Shadow @Final @Mutable protected static AxisAlignedBB BOTTOM_AABB = new AxisAlignedBB(0.0D, 0.0D, 0.0D, 1.0D, 0.1825D, 1.0D);
+    @Shadow @Final @Mutable protected static AxisAlignedBB TOP_AABB = new AxisAlignedBB(0.0D, 0.8175D, 0.0D, 1.0D, 1.0D, 1.0D);
 
     public MixinBlockTrapDoor(Material materialIn) {
         super(materialIn);
