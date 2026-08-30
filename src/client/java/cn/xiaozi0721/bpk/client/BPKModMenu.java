0c2ec6af0dcc088b73be15407ec9833be522f084
xiaozi0721 <1793116683@qq.com>
2026-08-30 17:08:34 +0800

    Fix ModMenu config screen: use AutoConfigClient.getConfigScreen
    AutoConfig.getConfigScreen was removed in cloth-config 26.2.155; the
    screen supplier now lives in me.shedaniel.autoconfig.AutoConfigClient.

--- src/client/java/cn/xiaozi0721/bpk/client/BPKModMenu.java
+++ src/client/java/cn/xiaozi0721/bpk/client/BPKModMenu.java
@@ -3,11 +3,11 @@ package cn.xiaozi0721.bpk.client;
 import cn.xiaozi0721.bpk.config.BPKConfig;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.AutoConfigClient;
 
 public class BPKModMenu implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> AutoConfig.getConfigScreen(BPKConfig.class, parent).get();
+        return parent -> AutoConfigClient.getConfigScreen(BPKConfig.class, parent).get();
     }
 }
